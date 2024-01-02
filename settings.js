ff9f7b5115ddf148c5ea3a4c12276110639ba8f1
RealRevolt <97731272+RealRevolt@users.noreply.github.com>
2024-01-02 13:31:10 +0700

    Puki

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Revolt'// yourname
+global.MONGO_DB_URI = "mongodb+srv://imrvlt8:Namakuhacker88@xontol.az89rke.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "imrvlt8@gmail.com" //email
+global.email_password = "kkxy bypf ygqi qkxa" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
